f26f6c60168cc045960540876e8c090fe7157e1c
VolumiDev <diego.martga@gmail.com>
2024-12-10 06:31:23 +0100

    usamos las routes para navegar entre distitos elementos de la web

---
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -0,0 +1,20 @@
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'shared-search-box',
+  templateUrl: './search-box.component.html',
+  styles: ``
+})
+export class SearchBoxComponent {
+
+  @Input()
+  public placeholder: string = '';
+
+  @Output()
+  onValue = new EventEmitter<string>();  
+
+  emitValue(value: string){
+    this.onValue.emit(value);
+  }
+
+}
